56c949b2d10967e7b76f066e5ea5397bf9966122
Brett Ian Balogh <giantmolecules@gmail.com>
2023-09-18 19:54:33 -0500

    neopixel_readout

--- neopixel_readout/neopixel_readout.ino
+++ neopixel_readout/neopixel_readout.ino
@@ -11,6 +11,12 @@
 //----------------------------------------------------------------//
 
 #include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+
+// Create a TFT object
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 // Which pin on the ESP32 is connected to the NeoPixels?
 #define PIN        33 // 
@@ -30,6 +36,25 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
 
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  // default text size
+  tft.setTextSize(2);
+
+  // set text foreground and background colors
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   pixels.clear(); // Set all pixel colors to 'off'
 }
@@ -40,7 +65,7 @@ void loop() {
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
-  
+
   // Count up
   for (int c = 0; c < 256; c++) {
     for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
@@ -51,6 +76,16 @@ void loop() {
 
     }
     pixels.show();   // Send the updated pixel colors to the hardware.
+    
+    tft.setCursor(0,0);
+    tft.print("R: ");
+    tft.print(255 - c);
+    tft.print(" G: ");
+    tft.print(0);
+    tft.print(" B: ");
+    tft.print(c);
+    tft.print(" ");
+    
     delay(DELAYVAL); // Pause before next pass through loop
   }
 
@@ -64,9 +99,19 @@ void loop() {
 
     }
     pixels.show();   // Send the updated pixel colors to the hardware.
+
+    tft.setCursor(0,0);
+    tft.print("R: ");
+    tft.print(255 - c);
+    tft.print(" G: ");
+    tft.print(0);
+    tft.print(" B: ");
+    tft.print(c);
+    tft.print(" ");
+
     delay(DELAYVAL); // Pause before next pass through loop
   }
-  
+
 }
 
 //----{END}------------------------------------------------------//
